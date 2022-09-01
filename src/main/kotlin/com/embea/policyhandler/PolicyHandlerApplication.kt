2c162e8c46c24a9daf178967fe34c0203112efcf
Sid <siddhartha@joinvitamin.com>
2022-09-01 23:10:09 +0200

    chore: initialize project

---
+++ src/main/kotlin/com/embea/policyhandler/PolicyHandlerApplication.kt
@@ -0,0 +1,11 @@
+package com.embea.policyhandler
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PolicyHandlerApplication
+
+fun main(args: Array<String>) {
+	runApplication<PolicyHandlerApplication>(*args)
+}
---
+++ src/test/kotlin/com/embea/policyhandler/PolicyHandlerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.embea.policyhandler
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PolicyHandlerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
